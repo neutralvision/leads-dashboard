6e0419277e36b8394962be315dcf794693cd2953
neutralvision <jesus.munoz9917@yahoo.com>
2024-05-21 23:27:09 -0700

    basic get and update, along tests

--- README.md
+++ README.md
@@ -65,10 +65,88 @@ be allowed so will allow all fields to be updated
 
 - Run `pip install -r requirements.txt`
 - Run `uvicorn main:app --reload`
-- 
-- Test Curl: `curl http://localhost:8000`
 
-- Test POST
+App is now running. Below you will find some tests to run
+
+
+- Hello World:
+Sanity test. verify a hello world response
+
+```
+curl http://localhost:8000
+
+Response:
+"hello world!"
+
+```
+
+- Test CREATE
 ```
 curl -X POST "http://127.0.0.1:8000/create" -H "Content-Type: application/json" -d '{"FIRST_NAME": "John", "LAST_NAME": "Doe", "EMAIL": "john.doe@example.com", "RESUME": "This is a resume"}'
+
+Response:
+1
+
+Info:
+Creates a new Lead with name John Doe. The return is the lead_id of our new lead
+
+```
+- Test UPDATE
+```
+curl -X POST "http://127.0.0.1:8000/update?lead_id=1" -H "Content-Type: application/json" -d '{"FIRST_NAME": "Jeff", "LAST_NAME": "Meff", "EMAIL": "john.doe@example.com", "REACHED_OUT": "This is a resume"}'
+
+Response:
+1
+
+Info:
+Updates our leads name to Jeff Meff, adds a simple resume, 
+and changes status to REACHED_OUT
+
+```
+- Test GET with Lead_id
+```
+curl "http://localhost:8000/get?lead_id=1"
+
+Response:
+[{"RESUME":"This is a resumeeee","LAST_NAME":"Meff","EMAIL":"john.doe@example.com","LEAD_ID":1,"FIRST_NAME":"Jeff","STATE":"PENDING"}]%
+
+Info:
+Retrieves our updated lead 1. note the name, resume and status change
+```
+
+- Test GET with invalid Lead_id
+```
+curl "http://localhost:8000/get?lead_id=7"
+
+Response:
+{"detail":"Lead with id 7 not found"}
+
+Info:
+Lead id 7 does not exist so gives us gentle reminder
+
+```
+
+- Test CREATE for a second lead
+```
+curl -X POST "http://127.0.0.1:8000/create" -H "Content-Type: application/json" -d '{"FIRST_NAME": "Apple", "LAST_NAME": "Bye", "EMAIL": "appleBye@gmail.com"}'
+
+
+Response:
+2
+
+Info:
+We have now added our second lead. the service returns our associated lead id
+
+```
+
+
+- Test GET without Lead_id
+```
+curl "http://localhost:8000/get"
+
+Response:
+[Jeff, Apple lead objects]
+
+Info: 
+Returns all leads in database. So far we only have 2
 ```
--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
+from typing import Optional
+
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from db import get_db
-from models import Lead, LeadState, LeadCreate
+from models import Lead, LeadCreate
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "hello world!"
 
 
 @app.post("/create")
-async def root(lead: LeadCreate, db: Session = Depends(get_db)):
+async def create_leads(lead: LeadCreate, db: Session = Depends(get_db)):
 
     new_lead = Lead(
         FIRST_NAME=lead.FIRST_NAME,
         LAST_NAME=lead.LAST_NAME,
         EMAIL=lead.EMAIL,
-        RESUME=lead.RESUME,
-        STATE=LeadState.PENDING
+        RESUME=lead.RESUME
     )
 
     try:
@@ -37,10 +38,44 @@ async def root(lead: LeadCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/get")
-async def root():
-    return "not implemented"
+async def get_leads(lead_id: Optional[int] = None, db: Session = Depends(get_db)):
+
+    # query with specific lead_id if provided
+    if lead_id is not None:
+        lead = db.query(Lead).filter(Lead.LEAD_ID == lead_id).first()
+        if not lead:
+            raise HTTPException(status_code=404, detail="Lead with id {} not found".format(lead_id))
+        return [lead]
+
+    # if no lead_id provided, query all
+    leads = db.query(Lead).all()
+
+    return leads
 
 
 @app.post("/update")
-async def root():
-    return "not implemented"
+async def update_leads(lead: LeadCreate, lead_id: int, db: Session = Depends(get_db)):
+
+    # grab existing entry
+    lead_to_update = db.query(Lead).filter(Lead.LEAD_ID == lead_id).first()
+
+    if not lead_to_update:
+        raise HTTPException(status_code=404, detail="Lead with id {} not found".format(lead.LEAD_ID))
+
+    # map values that need updating. only resume is optional
+    lead_to_update.FIRST_NAME = lead.FIRST_NAME
+    lead_to_update.LAST_NAME = lead.LAST_NAME
+    lead_to_update.EMAIL = lead.EMAIL
+    if lead.RESUME:
+        lead_to_update.RESUME = lead.RESUME
+
+    try:
+        db.commit()
+    except Exception as e:
+        print("error with /update with lead_id: {}".format(lead.id))
+        print(e)
+        raise HTTPException(status_code=500, detail="Unknown error with updating lead")
+
+    print("successfully updated lead with id: {}".format(lead_to_update.LEAD_ID))
+    return lead_to_update.LEAD_ID
+
